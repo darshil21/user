16ffc0ace589ffe2df9e4c273d66df23691b06e4
Darshil Patel <darshil_21@yahoo.in>
2018-01-25 18:33:33 +0530

    new dir

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { routing } from './app.routing';
+
+import { HomedataService } from './homedata.service';
+import { LogindataService } from "./logindata.service";
+import { RegisterdataService } from "./registerdata.service";
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { DetailproductComponent } from './detailproduct/detailproduct.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { ComboComponent } from './combo/combo.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutusComponent,
+    FooterComponent,
+    ContactusComponent,
+    DetailproductComponent,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent,
+    ComboComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
+  providers: [HomedataService,LogindataService,RegisterdataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
